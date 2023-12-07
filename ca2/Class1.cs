0c8205edf34c5cbaecf013e87fc3e033ac107bf9
daniel <s00236328@atu.ie>
2023-12-07 12:25:42 +0000

    Q1i
    unskipped i

--- ca2/Class1.cs
+++ ca2/Class1.cs
@@ -2,8 +2,9 @@
 
 public class Player
 {
-		public string Name { get; set; }
-		public string ResultRecord { get; set; }
+	public string Name { get; set; }
+	public string ResultRecord { get; set; }
+	public int Rating { get; set; }
 
 	public Player()
 	{
--- ca2/MainWindow.xaml.cs
+++ ca2/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.Pkcs;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -27,24 +28,25 @@ namespace ca2
         Team t3 = new Team() { Name = "Spain" };
 
         // French players
-        Player p1 = new Player() { Name = "Marie", ResultRecord = "WWDDL" };
-        Player p2 = new Player() { Name = "Claude", ResultRecord = "DDDLW" };
-        Player p3 = new Player() { Name = "Antoine", ResultRecord = "LWDLW" };
+        Player p1 = new Player() { Name = "Marie", ResultRecord = "WWDDL", Rating = 0 };
+        Player p2 = new Player() { Name = "Claude", ResultRecord = "DDDLW", Rating = 0 };
+        Player p3 = new Player() { Name = "Antoine", ResultRecord = "LWDLW", Rating = 0 };
 
         // Italian players
-        Player p4 = new Player() { Name = "Marco", ResultRecord = "WWDLL" };
-        Player p5 = new Player() { Name = "Giovanni", ResultRecord = "LLLLD" };
-        Player p6 = new Player() { Name = "Valentina", ResultRecord = "DLWWW" };
+        Player p4 = new Player() { Name = "Marco", ResultRecord = "WWDLL", Rating = 0 };
+        Player p5 = new Player() { Name = "Giovanni", ResultRecord = "LLLLD", Rating = 0 };
+        Player p6 = new Player() { Name = "Valentina", ResultRecord = "DLWWW", Rating = 0 };
 
         // Spanish players
-        Player p7 = new Player() { Name = "Maria", ResultRecord = "WWWWW" };
-        Player p8 = new Player() { Name = "Jose", ResultRecord = "LLLLL" };
-        Player p9 = new Player() { Name = "Pablo", ResultRecord = "DDDDD" };
+        Player p7 = new Player() { Name = "Maria", ResultRecord = "WWWWW", Rating = 0 };
+        Player p8 = new Player() { Name = "Jose", ResultRecord = "LLLLL", Rating = 0 };
+        Player p9 = new Player() { Name = "Pablo", ResultRecord = "DDDDD", Rating = 0 };
 
         public MainWindow()
         {
             InitializeComponent();
             GetData();
+            Calculate();
         }
 
         public void GetData()
@@ -66,8 +68,151 @@ namespace ca2
             t3.Players.Add(p9);
         }
 
+        public void Calculate()
+        {
+            p1.Rating = 0;
+            p2.Rating = 0;
+            p3.Rating = 0;
+            p4.Rating = 0;
+            p5.Rating = 0;
+            p6.Rating = 0;
+            p7.Rating = 0;
+            p8.Rating = 0;
+            p9.Rating = 0;
+
+            foreach (char W in p1.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p1.Rating += 3;
+                }
+            }
+            foreach (char W in p2.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p2.Rating += 3;
+                }
+            }
+            foreach (char W in p3.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p3.Rating += 3;
+                }
+            }
+            foreach (char W in p4.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p4.Rating += 3;
+                }
+            }
+            foreach (char W in p5.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p5.Rating += 3;
+                }
+            }
+            foreach (char W in p6.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p6.Rating += 3;
+                }
+            }
+            foreach (char W in p7.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p7.Rating += 3;
+                }
+            }
+            foreach (char W in p8.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p8.Rating += 3;
+                }
+            }
+            foreach (char W in p9.ResultRecord)
+            {
+                if (W == 'W')
+                {
+                    p9.Rating += 3;
+                }
+            }
+
+
+            foreach (char D in p1.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p1.Rating ++;
+                }
+            }
+            foreach (char D in p2.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p2.Rating ++;
+                }
+            }
+            foreach (char D in p3.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p3.Rating++;
+                }
+            }
+            foreach (char D in p4.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p4.Rating++;
+                }
+            }
+            foreach (char D in p5.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p5.Rating++;
+                }
+            }
+            foreach (char D in p6.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p6.Rating++;
+                }
+            }
+            foreach (char D in p7.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p7.Rating++;
+                }
+            }
+            foreach (char D in p8.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p8.Rating++;
+                }
+            }
+            foreach (char D in p9.ResultRecord)
+            {
+                if (D == 'D')
+                {
+                    p9.Rating++;
+                }
+            }
+        }
+
         private void LBXTeams_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            Calculate();
             // Get the selected team
             string selectedTeam = LBXTeams.SelectedItem as string;
 
@@ -78,7 +223,7 @@ namespace ca2
                 LBXPlayers.Items.Clear(); // Clear existing items
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t2.Name)
@@ -87,7 +232,7 @@ namespace ca2
                 LBXPlayers.Items.Clear(); // Clear existing items
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t3.Name)
@@ -96,7 +241,7 @@ namespace ca2
                 LBXPlayers.Items.Clear(); // Clear existing items
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else
@@ -120,12 +265,13 @@ namespace ca2
                 p1.ResultRecord += "W";
                 p2.ResultRecord += "W";
                 p3.ResultRecord += "W";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t1.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t2.Name)
@@ -136,12 +282,13 @@ namespace ca2
                 p4.ResultRecord += "W";
                 p5.ResultRecord += "W";
                 p6.ResultRecord += "W";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t2.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t3.Name)
@@ -152,12 +299,13 @@ namespace ca2
                 p7.ResultRecord += "W";
                 p8.ResultRecord += "W";
                 p9.ResultRecord += "W";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t3.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
         }
@@ -176,12 +324,13 @@ namespace ca2
                 p1.ResultRecord += "D";
                 p2.ResultRecord += "D";
                 p3.ResultRecord += "D";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t1.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t2.Name)
@@ -192,12 +341,13 @@ namespace ca2
                 p4.ResultRecord += "D";
                 p5.ResultRecord += "D";
                 p6.ResultRecord += "D";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t2.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t3.Name)
@@ -208,12 +358,13 @@ namespace ca2
                 p7.ResultRecord += "D";
                 p8.ResultRecord += "D";
                 p9.ResultRecord += "D";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t3.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
         }
@@ -232,12 +383,13 @@ namespace ca2
                 p1.ResultRecord += "L";
                 p2.ResultRecord += "L";
                 p3.ResultRecord += "L";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t1.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t2.Name)
@@ -248,12 +400,13 @@ namespace ca2
                 p4.ResultRecord += "L";
                 p5.ResultRecord += "L";
                 p6.ResultRecord += "L";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t2.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
             else if (selectedTeam == t3.Name)
@@ -264,12 +417,13 @@ namespace ca2
                 p7.ResultRecord += "L";
                 p8.ResultRecord += "L";
                 p9.ResultRecord += "L";
+                Calculate();
 
                 LBXPlayers.Items.Clear();
                 List<Player> players = t3.Players;
                 foreach (Player player in players)
                 {
-                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord);
+                    LBXPlayers.Items.Add(player.Name + " - " + player.ResultRecord + " - " + player.Rating);
                 }
             }
         }
